3e912a051631dd887671942dbd379967f6206927
rosendo <42633099+rosendolu@users.noreply.github.com>
2024-02-21 20:59:08 +0800

    build: use relative bundle path

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    envDir: 'env',
+    envPrefix: ['__APP__'],
+    base: './',
+    optimizeDeps: {
+        exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
+    },
+
     resolve: {
         alias: {
             '@assets': './src/assets',
+            // @ts-ignore
+            '@': path.resolve(__dirname, './src/'),
         },
-    },
-    optimizeDeps: {
-        exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
     },
     server: {
         // headers: {
