d8e7dd2cd486884e78592f529805bc6bf356047f
rosendo <42633099+rosendolu@users.noreply.github.com>
2024-02-21 21:28:34 +0800

    chore:

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import App from '../App';
 import VideoPosterCapture from '../components/VideoPosterCapture';
 import Canvas from '../components/canvas';
@@ -99,4 +99,4 @@ export const routerConfig = [
         ],
     },
 ];
-export const router = createBrowserRouter(routerConfig);
+export const router = createHashRouter(routerConfig);
